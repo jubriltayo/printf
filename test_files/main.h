fe68c7018cdeded9d9042aac99d1c94f98334e6d
Paodt <odehpatrick255@gmail.com>
2022-10-16 04:31:48 -0700

    functions

---
+++ test_files/main.h
@@ -0,0 +1,20 @@
+#ifdef PRINT_F
+#define PRINT_F
+
+/* main function */
+
+int _printf(const char *format, ...);
+
+/* -putchar function */
+
+int _putchar(char c);
+
+/* sub functions */
+
+int print_char(char c);
+int print_strings(char *string);
+int print_decimal(int num);
+
+
+
+#endif
---
+++ test_files/print_char.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_char - function that prints character
+ * @c: character variable
+ * Return: 0
+ */
+
+int print_char(char c)
+{
+	int x;
+
+	x = _putchar(c);
+	return (c);
+	return (0);
+}
