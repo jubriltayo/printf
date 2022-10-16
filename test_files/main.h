c9fd3f604bdd5aa823f66d5b209b4459ba66f3db
Tayo Jubril <jubriltayo@gmail.com>
2022-10-16 12:35:54 -0700

    update

---
+++ test_files/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* main function */
+int _printf(const char *format, ...);
+
+/* _putchar function */
+int _putchar(char c);
+
+/* sub functions */
+int print_char(char c);
+int print_strings(char *string);
+int print_decimal(int num);
+
+#endif
